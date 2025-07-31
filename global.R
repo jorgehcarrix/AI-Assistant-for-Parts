7e36ccfda9f6a534a3a0bd326087e6937051c7f1
jorgehcarrix <jorge.hernandez@carrix.com>
2025-07-31 15:24:22 -0600

    Add files via upload

---
+++ global.R
@@ -0,0 +1,19 @@
+
+library(shiny)
+library(dplyr)
+library(httr)
+library(dplyr)
+library(jsonlite)
+library(shinyWidgets)
+library(typedjs)
+library(lubridate)
+library(bslib)
+library(reactable)
+library(reactablefmtr)
+
+
+
+url <- "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" 
+api_key <- "AIzaSyDoepx_hDL232da2Tk4eYSZOjEf4dVKBoA"  
+
+
